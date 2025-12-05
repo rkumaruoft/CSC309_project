ff811ec2b315b0db57d624b384d985209c115561
reeshav <reeshav.kumar@mail.utoronto.ca>
2025-12-04 21:13:58 -0500

    Changes to db

--- backend/config/env.js
+++ backend/config/env.js
@@ -6,6 +6,7 @@ export const JWT_SECRET = process.env.JWT_SECRET;
 
 // Frontend
 export const FRONTEND_URL = process.env.FRONTEND_URL;
+export const DATABASE_URL = process.env.DATABASE_URL;
 
 // Resend (new)
 export const RESEND_API_KEY = process.env.RESEND_API_KEY;
